49e37479b0d9fc3c88aaea65623f4d065c5428cf
Jan Safarik <jsafarik@redhat.com>
2023-12-24 17:19:07 +0100

    [Three] Make sure WebGL support is available

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
-	export let data
+	import WebGL from 'three/addons/capabilities/WebGL.js';
+
+	export let data;
 </script>
 
-<ul>
-	{#each data.keys as key}
-		<li><a href="/playground/{key}">{key}</a></li>
-	{/each}
-</ul>
+{#if WebGL.isWebGLAvailable()}
+	<ul>
+		{#each data.keys as key}
+			<li><a href="/playground/{key}">{key}</a></li>
+		{/each}
+	</ul>
+{:else}
+	<span>WebGL is not supported</span>
+{/if}
