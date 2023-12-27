3145fc47efd29c67d3a4700788dca6ee6fa61751
Jan Safarik <jsafarik@redhat.com>
2023-12-27 14:35:01 +0100

    [Playground] Make root menu a little better

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,8 +1,20 @@
-export function load(): { keys: string[] } {
-	const scripts = import.meta.glob('./playground/*/+page.svelte');
-	const keys = Object.keys(scripts).map((key) => key.replace("./playground/", "").replace(/\/.*/, ""));
+import type { Node } from '$lib/node';
 
-	return {
-		keys
-	};
+export function load(): Node {
+	const scripts = import.meta.glob('./playground/**/+page.svelte');
+	const keys = Object.keys(scripts)
+		.map((key) => key.replace('./playground/', '').replace('/+page.svelte', ''))
+		.reduce((acc: Node, cur) => {
+			let accPointer = acc;
+			cur.split('/').forEach((key, i) => {
+				if (i == cur.split('/').length - 1) {
+					accPointer[key] = cur;
+				} else {
+					accPointer[key] = accPointer[key] || {};
+					accPointer = accPointer[key] as Node;
+				}
+			});
+			return acc;
+		}, {});
+	return keys;
 }
