28fda196ee03942a5250310f5d1a4fd4e56d6154
Jan Safarik <jsafarik@redhat.com>
2023-12-27 15:04:03 +0100

    [Three] Add sketch with simple DOM+CSS text

---
+++ src/routes/playground/text/DOM+CSS/+page.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import * as THREE from 'three';
+	import { Scene } from '$lib/scene';
+	import Renderer from '$lib/Renderer.svelte';
+
+	let cube: THREE.Mesh;
+
+	const setup = (scene: Scene) => {
+		const geometry = new THREE.BoxGeometry();
+		const material = new THREE.MeshBasicMaterial({ color: 0xff5012 });
+		cube = new THREE.Mesh(geometry, material);
+		scene.addObject(cube);
+		scene.camera.position.z = 4;
+	};
+
+	const draw = () => {
+		cube.rotation.x += 0.01;
+		cube.rotation.y += 0.01;
+	};
+</script>
+
+<span>DOM + CSS</span>
+<Renderer {setup} {draw} />
+
+<style>
+	span {
+		position: absolute;
+		top: 10px;
+		right: 10px;
+		color: white;
+	}
+</style>
